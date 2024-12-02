c17f96fa19e5d0061f61eb0abaa3b09ec13651b9
Bálint Király <balintkiraly.dev@gmail.com>
2024-12-02 07:05:41 +0100

    Initial Commit

---
+++ AssignmentTracker/AssignmentTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentTrackerApp.swift
+//  AssignmentTracker
+//
+//  Created by Bálint Király on 2024. 12. 02..
+//
+
+import SwiftUI
+
+@main
+struct AssignmentTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AssignmentTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AssignmentTracker
+//
+//  Created by Bálint Király on 2024. 12. 02..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
